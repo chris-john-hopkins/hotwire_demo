28daa8943a126308357d8ef7855f4d5324c608d6
Chris Hopkins <42541529+chris-john-hopkins@users.noreply.github.com>
2022-07-30 14:14:56 +0100

    chat functionality

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,7 @@
 Rails.application.routes.draw do
+  resources :rooms do
+    resources :messages
+  end
   resources :books
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
